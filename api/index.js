433ea0b695047d9b74e09ab880ba38436790ab75
theotime2005 <theotime.berthod@epitech.digital>
2025-07-16 19:39:21 +0200

    feat(api): add the start file

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {healthCheckRoute} from "./src/shared/routes/health-check-route.js";
+
+const app = express();
+
+app.use(healthCheckRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
