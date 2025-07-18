62a2766ec19421b80231eeb27b10b4ab5448f7a7
theotime2005 <theotime.berthod@epitech.digital>
2025-07-19 00:01:14 +0200

    tech(api): create database builder

---
+++ api/db/database-builder/factory/build-user.js
@@ -0,0 +1,27 @@
+import { createPassword } from "../../../src/authentication/services/password.service.js";
+import { USER_TYPES } from "../../../src/shared/constants.js";
+import { knex } from "../../knex-database-connection.js";
+
+
+async function buildUser({
+  firstname = "John",
+  lastname = "Doe",
+  email = "john.doe@example.net",
+  password = "password",
+  userType = USER_TYPES.MASTER_GUIDE_DOG,
+  lastLoggedAt = null,
+} = {}) {
+  const hashedPassword = await createPassword(password);
+  const [values] = await knex("users").insert({
+    firstname,
+    lastname,
+    email,
+    hashedPassword,
+    userType,
+    lastLoggedAt,
+  }).returning("*");
+
+  return values;
+}
+
+export { buildUser };
---
+++ api/db/database-builder/index.js
@@ -0,0 +1,9 @@
+import { buildUser } from "./factory/build-user.js";
+
+const databaseBuilder = {
+  factory: {
+    buildUser,
+  },
+};
+
+export { databaseBuilder };
