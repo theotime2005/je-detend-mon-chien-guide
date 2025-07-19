c2927de53232e1aa31999a740eba41951e6cdde4
theotime2005 <theotime.berthod@epitech.digital>
2025-07-19 19:06:22 +0200

    feat(api): add a controller for login

---
+++ api/src/authentication/controllers/login-controller.js
@@ -0,0 +1,48 @@
+import Joi from "joi";
+
+import { checkSchema } from "../../shared/middlewares/checkSchema.js";
+import * as loginRepository from "../repositories/login.repository.js";
+import { checkPassword } from "../services/password.service.js";
+import { encodedToken } from "../services/token.service.js";
+
+const loginSchema = Joi.object({
+  email: Joi.string().required().email(),
+  password: Joi.string().required(),
+});
+
+async function loginController(req, res) {
+  if (!(await checkSchema(req, res, loginSchema))) return;
+  try {
+    const foundUser = await loginRepository.findUserByEmail(req.body.email);
+    if (!(await checkPassword(req.body.password, foundUser.hashedPassword))) {
+      throw new Error("Invalid credentials");
+    }
+    await loginRepository.updateLastLoggedAt(foundUser.id);
+    const token = await encodedToken({
+      userId: foundUser.id,
+    });
+    return res.status(200).json({
+      data: {
+        userId: foundUser.id,
+        token,
+      },
+    });
+  } catch (err) {
+    switch (err.message) {
+    case "Invalid credentials":
+      return res.status(401).json({
+        error: {
+          message: "Invalid credentials",
+        },
+      });
+    default:
+      return res.status(500).json({
+        error: {
+          message: "Internal server error",
+        },
+      });
+    }
+  }
+}
+
+export { loginController };
