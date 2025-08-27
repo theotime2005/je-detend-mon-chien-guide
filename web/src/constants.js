777f9e5a8e02e078934523f8132915e3b8cd408e
theotime2005 <theotime.berthod@epitech.digital>
2025-08-27 16:11:51 +0200

    tech(web): add constants

---
+++ web/src/constants.js
@@ -0,0 +1,12 @@
+const USER_TYPES = {
+  MASTER_GUIDE_DOG: {
+    type: "master_guide_dog",
+    text: "Maître de chien guide",
+  },
+  COMPANION: {
+    type: "companion",
+    text: "Accompagnateur",
+  },
+};
+
+export { USER_TYPES };
