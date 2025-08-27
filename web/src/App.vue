cbb83e09fbfbd0c9e78c388e841cc99d3cd18528
theotime2005 <theotime.berthod@epitech.digital>
2025-08-27 22:32:55 +0200

    feat(web): create a navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 
 import FooterComponent from "@/components/FooterComponent.vue";
+import NavbarComponent from "@/components/NavbarComponent.vue";
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NavbarComponent/>
   </header>
 
   <RouterView />
---
+++ web/src/components/NavbarComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+import routerList from "@/router/router-list.js";
+
+const routes = ref([]);
+
+function getAvailableroutes() {
+  routerList.map((route) => {
+    if (route.meta && route.meta.navbar) {
+      routes.value.push(route);
+    }
+  });
+}
+
+onMounted(() => {
+  getAvailableroutes();
+});
+</script>
+
+<template>
+  <nav aria-label="Menu de navigation">
+    <menu>
+      <RouterLink
+        class="route"
+        v-for="route in routes" :key="route.path"
+        :to="route.path"
+        >{{route.name}}</RouterLink>
+    </menu>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
