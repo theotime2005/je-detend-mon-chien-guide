5233c0b7202290f624504338143283d949be533e
theotime2005 <theotime.berthod@epitech.digital>
2025-08-27 19:04:38 +0200

    refactor(api): place authentication routes in one file

--- api/server.js
+++ api/server.js
@@ -2,8 +2,7 @@ import cors from "cors";
 import express from "express";
 import path from "path";
 
-import { loginRoute } from "./src/authentication/routes/login-route.js";
-import { registerRoute } from "./src/authentication/routes/register-route.js";
+import authentication from "./src/authentication/routes.js";
 import { healthCheckRoute } from "./src/shared/routes/health-check-route.js";
 
 const server = express();
@@ -12,7 +11,6 @@ server.use(cors());
 server.use(express.json());
 server.use(express.static(path.join(process.cwd(), "dist")));
 server.use(healthCheckRoute);
-server.use(registerRoute);
-server.use(loginRoute);
+server.use(authentication);
 
 export default server;
