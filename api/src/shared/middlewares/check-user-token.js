61e8c917929b0e60aee38e74636c453c3b73a500
theotime2005 <theotime.berthod@epitech.digital>
2025-08-31 22:58:37 +0200

    tech(api): add a middleware to check user token

---
+++ api/src/shared/middlewares/check-user-token.js
@@ -0,0 +1,26 @@
+import { decodedToken } from "../../authentication/services/token.service.js";
+import { ERRORS } from "../constants.js";
+
+/**
+ * Authorize the access from token
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<*>}
+ */
+async function checkUserToken(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: ERRORS.NO_TOKEN_PROVIDED });
+  }
+  const token = req.headers["authorization"].split(" ")[1];
+
+  try {
+    const decoded = await decodedToken(token);
+    req.user = decoded;
+    return next();
+  } catch {
+    return res.status(401).json({ message: ERRORS.INVALID_TOKEN });
+  }
+}
+
+export { checkUserToken };
---
+++ api/tests/shared/integration/middlewares/check-user-token.spec.js
@@ -0,0 +1,71 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { encodedToken } from "../../../../src/authentication/services/token.service.js";
+import { ERRORS } from "../../../../src/shared/constants.js";
+import { checkUserToken } from "../../../../src/shared/middlewares/check-user-token.js";
+
+describe("Integration | Middlewares | Check user token", () => {
+  let res, next;
+
+  beforeEach(() => {
+    res = {
+      status: vi.fn().mockReturnThis(),
+      json: vi.fn(),
+    };
+    next = vi.fn();
+  });
+
+  it("should return 401 when there is no token ", async () => {
+    // given
+    const req = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    // when
+    await checkUserToken(req, res, next);
+
+    // then
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: ERRORS.NO_TOKEN_PROVIDED });
+  });
+
+  it("should return 401 when token is invalid", async () => {
+    // given
+    const req = {
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": "Bearer abcdefg",
+      },
+    };
+
+    // when
+    await checkUserToken(req, res, next);
+
+    // then
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: ERRORS.INVALID_TOKEN });
+  });
+
+  it("should return next and update req when token is valid", async () => {
+    // given
+    const token = await encodedToken({
+      message: "test succeeded",
+    });
+    const req = {
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": `Bearer ${token}`,
+      },
+    };
+
+    // when
+    await checkUserToken(req, res, next);
+
+    // then
+    expect(next).toHaveBeenCalled();
+    expect(req.user).toBeDefined();
+    expect(req.user.message).toBe("test succeeded");
+  });
+});
