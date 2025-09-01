d4c02460a6f4e4d7d47710ae68e27e481ca260cd
theotime2005 <theotime.berthod@epitech.digital>
2025-09-01 16:33:04 +0200

    tech(web): add checking for user

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import routes from "@/router/router-list.js";
+import { useAuthenticatedStore } from "@/stores/authenticated.js";
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const authenticated = useAuthenticatedStore();
+  await authenticated.getAuthenticated();
+  if (to.meta.requireAuth) {
+
+    if (!authenticated.isLogin()) {
+      return next("/authentication/login");
+    }
+  }
+  return next();
 });
 
 export default router;
