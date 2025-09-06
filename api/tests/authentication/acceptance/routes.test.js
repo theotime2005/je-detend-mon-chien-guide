ad8fbaf207114ac6ded2ef809d596de857efe148
theotime2005 <theotime.berthod@epitech.digital>
2025-09-06 23:30:31 +0200

    feat(api): create route to activate user

--- api/tests/authentication/acceptance/routes.test.js
+++ api/tests/authentication/acceptance/routes.test.js
@@ -4,76 +4,98 @@ import { describe, expect, it } from "vitest";
 import { databaseBuilder } from "../../../db/database-builder/index.js";
 import { knex } from "../../../db/knex-database-connection.js";
 import server from "../../../server.js";
+import { encodedToken } from "../../../src/authentication/services/token.service.js";
 import { ERRORS, USER_TYPES } from "../../../src/shared/constants.js";
 
 describe("Acceptance | Authentication | Register", () => {
 
-  describe("POST /api/authentication/register", () => {
-    it("should return 200 http status code", async () => {
-      // given
-      const body = {
-        firstname: "John",
-        lastname: "Doe",
-        email: "john.doe@example.net",
-        password: "password123",
-        userType: USER_TYPES.MASTER_GUIDE_DOG,
-      };
+  describe("registration", () => {
+    describe("POST /api/authentication/register", () => {
+      it("should return 200 http status code", async () => {
+        // given
+        const body = {
+          firstname: "John",
+          lastname: "Doe",
+          email: "john.doe@example.net",
+          password: "password123",
+          userType: USER_TYPES.MASTER_GUIDE_DOG,
+        };
 
-      // when
-      const response = await request(server)
-        .post("/api/authentication/register")
-        .send(body);
+        // when
+        const response = await request(server)
+          .post("/api/authentication/register")
+          .send(body);
 
-      // then
-      expect(response.statusCode).toBe(201);
-      const user = await knex("users").where({ email: body.email }).first();
-      expect(user).toBeDefined();
-      expect(user.firstname).toBe(body.firstname);
-      expect(user.lastname).toBe(body.lastname);
-      expect(user.email).toBe(body.email);
-      expect(user.userType).toBe(body.userType);
+        // then
+        expect(response.statusCode).toBe(201);
+        const user = await knex("users").where({ email: body.email }).first();
+        expect(user).toBeDefined();
+        expect(user.firstname).toBe(body.firstname);
+        expect(user.lastname).toBe(body.lastname);
+        expect(user.email).toBe(body.email);
+        expect(user.userType).toBe(body.userType);
+      });
+
+      it("should return 409 status code", async () => {
+        // given
+        const user = {
+          firstname: "Jane",
+          lastname: "Doe",
+          email: "jane.doe@example.net",
+          hashedPassword: "hashedPassword123",
+          userType: USER_TYPES.MASTER_GUIDE_DOG,
+        };
+        await knex("users").insert(user);
+
+        const body = {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          password: "password123",
+          userType: user.userType,
+        };
+        // when
+        const response = await request(server).post("/api/authentication/register").send(body);
+
+        // then
+        expect(response.statusCode).toBe(409);
+        expect(response.body.code).toBe("EXISTS");
+      });
+
+      it("should return 400 status if body is invalid", async () => {
+        // given
+        const body = {
+          firstname: "John",
+          lastname: "Doe",
+          password: "password123",
+        };
+
+        // when
+        const response = await request(server).post("/api/authentication/register").send(body);
+
+        // then
+        expect(response.statusCode).toBe(400);
+        expect(response.body.code).toBe(ERRORS.INVALID_DATA_FORMAT);
+      });
     });
 
-    it("should return 409 status code", async () => {
-      // given
-      const user = {
-        firstname: "Jane",
-        lastname: "Doe",
-        email: "jane.doe@example.net",
-        hashedPassword: "hashedPassword123",
-        userType: USER_TYPES.MASTER_GUIDE_DOG,
-      };
-      await knex("users").insert(user);
+    describe("PATCH /api/authentication/register", () => {
+      it("should activate user and return 200 http status code", async () => {
+        // given
+        const user = await databaseBuilder.factory.buildUser({ isActive: false });
+        const token = await encodedToken({ userId: user.id });
 
-      const body = {
-        firstname: user.firstname,
-        lastname: user.lastname,
-        email: user.email,
-        password: "password123",
-        userType: user.userType,
-      };
-      // when
-      const response = await request(server).post("/api/authentication/register").send(body);
+        // when
+        const response = await request(server)
+          .patch("/api/authentication/register")
+          .set("Authorization", `Bearer ${token}`)
+          .send();
 
-      // then
-      expect(response.statusCode).toBe(409);
-      expect(response.body.code).toBe("EXISTS");
-    });
-
-    it("should return 400 status if body is invalid", async () => {
-      // given
-      const body = {
-        firstname: "John",
-        lastname: "Doe",
-        password: "password123",
-      };
-
-      // when
-      const response = await request(server).post("/api/authentication/register").send(body);
-
-      // then
-      expect(response.statusCode).toBe(400);
-      expect(response.body.code).toBe(ERRORS.INVALID_DATA_FORMAT);
+        // then
+        expect(response.statusCode).toBe(204);
+        const activatedUser = await knex("users").where({ id: user.id }).first();
+        expect(activatedUser.isActive).toBe(true);
+      });
     });
   });
 
