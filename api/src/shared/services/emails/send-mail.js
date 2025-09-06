840fa2d0b1fbbe2b1298880a2b02b2974dffef29
theotime2005 <theotime.berthod@epitech.digital>
2025-09-06 16:27:49 +0200

    feat(api): create a mail sending service

---
+++ api/src/shared/services/emails/send-mail.js
@@ -0,0 +1,66 @@
+import nodemailer from "nodemailer";
+
+import { config } from "../../../../config.js";
+import { logger } from "../../logger.js";
+
+const { email } = config;
+
+async function sendMail(req) {
+  if (!req.to) {
+    throw new Error("Recipient email address is required");
+  }
+
+  const mailOptions = {
+    from: email.auth.user,
+    to: req.to,
+    subject: req.subject || "No Subject",
+  };
+
+  if (req.text) {
+    mailOptions.text = req.text;
+  } else if (req.html) {
+    mailOptions.html = req.html;
+  }
+
+  if (!email.enabled) {
+    logger.info("Email disabled. Mail not sent. Mail info:", mailOptions);
+    return {
+      info: "Email sending disabled",
+      data: mailOptions,
+    };
+  }
+  let transporter, testAccount;
+
+  if (email.testAccount) {
+    testAccount = await nodemailer.createTestAccount();
+    transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+  } else {
+    transporter = nodemailer.createTransport({
+      service: email.service,
+      port: email.port,
+      secure: email.secure,
+      auth: email.auth,
+    });
+  }
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    if (email.testAccount) {
+      logger.info("Email available on", nodemailer.getTestMessageUrl(info));
+    }
+    return info;
+  } catch (error) {
+    logger.error("Error sending email:", error);
+    throw error;
+  }
+}
+
+export { sendMail };
---
+++ api/tests/shared/unit/services/emails/send-mail.spec.js
@@ -0,0 +1,130 @@
+import nodemailer from "nodemailer";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { config } from "../../../../../config.js";
+import { sendMail } from "../../../../../src/shared/services/emails/send-mail.js";
+
+const { email } = config;
+
+vi.mock("nodemailer");
+
+describe("Unit | Shared | Services | Send mail", () => {
+  beforeEach(() => {
+    email.auth.user = "john.doe@example.net";
+    nodemailer.createTransport.mockReturnValue({
+      sendMail: vi.fn().mockResolvedValue(true),
+    });
+  });
+  describe("when mail is disabled", () => {
+    it("should return the email object with text", async () => {
+      // given
+      email.enabled = false;
+      const req = {
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        text: "This is the text",
+      };
+
+      // when
+      const result = await sendMail(req);
+
+      // then
+      expect(result).toEqual({
+        info: "Email sending disabled",
+        data: {
+          from: "john.doe@example.net",
+          to: "alex-terieur@example.net",
+          subject: "The subject",
+          text: "This is the text",
+        },
+      });
+    });
+
+    it("should return the email object with html", async () => {
+      // given
+      email.enabled = false;
+      const req = {
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        html: "This is the text",
+      };
+
+      // when
+      const result = await sendMail(req);
+
+      // then
+      expect(result).toEqual({
+        info: "Email sending disabled",
+        data: {
+          from: "john.doe@example.net",
+          to: "alex-terieur@example.net",
+          subject: "The subject",
+          html: "This is the text",
+        },
+      });
+    });
+  });
+
+  describe("when mail is enabled", () => {
+    beforeEach(() => {
+      email.enabled = true;
+    });
+    it("should send email correctly with text", async () => {
+      // given
+      const req = {
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        text: "This is the text",
+      };
+
+      // when
+      const result = await sendMail(req);
+
+      // then
+      expect(nodemailer.createTransport().sendMail).toHaveBeenCalledWith({
+        from: "john.doe@example.net",
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        text: "This is the text",
+      });
+      expect(result).toBe(true);
+    });
+
+    it("should send email correctly with html", async () => {
+      // given
+      const req = {
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        html: "This is the text",
+      };
+
+      // when
+      const result = await sendMail(req);
+
+      // then
+      expect(nodemailer.createTransport().sendMail).toHaveBeenCalledWith({
+        from: "john.doe@example.net",
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        html: "This is the text",
+      });
+      expect(result).toBe(true);
+    });
+
+    it("should return error", async () => {
+      // given
+      nodemailer.createTransport().sendMail.mockRejectedValue("Network error");
+      const req = {
+        to: "alex-terieur@example.net",
+        subject: "The subject",
+        html: "This is the text",
+      };
+
+      // when
+      const promise = sendMail(req);
+
+      // then
+      await expect(promise).rejects.toBe("Network error");
+    });
+  });
+});
